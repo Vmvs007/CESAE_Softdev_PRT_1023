8fb83571a6a76c65bca027e4298796762351e6aa
Vitor Santos <vmvs007@gmail.com>
2023-10-19 11:52:43 +0100

    FichaPratica01

--- FichasJava/src/FichaPratica01/Ex_02.java
+++ FichasJava/src/FichaPratica01/Ex_02.java
@@ -1,4 +1,14 @@
 package FichaPratica01;
 
+import java.util.Scanner;
+
 public class Ex_02 {
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner (obrigatório para todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+
+
+    }
 }
