2fc8c159e4dd297d5c4d4c3903ad0337b57160e3
Vitor Santos <vmvs007@gmail.com>
2023-10-20 16:09:59 +0100

    FichaPratica02

--- FichasJava/src/FichaPratica02/Ex_01.java
+++ FichasJava/src/FichaPratica02/Ex_01.java
@@ -1,4 +1,39 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex_01 {
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner (obrigatório para todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int num1, num2;
+
+        // Ler num1
+        System.out.print("Insira um numero: ");
+        num1= input.nextInt();
+
+        // Ler num2
+        System.out.print("Insira um numero: ");
+        num2= input.nextInt();
+
+        if(num1>num2){ // Maior: num1
+            System.out.println("Maior: " + num1);
+        }
+
+        if(num1<num2){ // Maior: num2
+            System.out.println("Maior: " + num2);
+        }
+
+        if(num1==num2){ // São iguais
+            System.out.println("São iguais");
+        }
+
+
+
+
+
+    }
 }
--- FichasJava/src/FichaPratica02/Ex_02.java
+++ FichasJava/src/FichaPratica02/Ex_02.java
@@ -1,4 +1,25 @@
 package FichaPratica02;
 
+import java.util.Scanner;
+
 public class Ex_02 {
+    public static void main(String[] args) {
+        // Instanciar o Scanner (obrigatório para todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double salario;
+
+        // Ler salario
+        System.out.print("Insira o salário: ");
+        salario= input.nextDouble();
+
+        if(salario<=15000){
+            System.out.println("Taxa 20%: "+ (salario*0.2));
+        }else{
+            System.out.println("Taxa 30%: "+(salario*0.3));
+        }
+
+
+    }
 }
