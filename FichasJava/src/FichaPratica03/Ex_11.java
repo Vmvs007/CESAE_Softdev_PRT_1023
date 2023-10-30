7b9a35810190b50a1f52b9c7574b9d708c49ccdc
Vitor Santos <vmvs007@gmail.com>
2023-10-30 14:31:14 +0000

    FichaPratica03

--- FichasJava/src/FichaPratica03/Ex_11.java
+++ FichasJava/src/FichaPratica03/Ex_11.java
@@ -1,4 +1,42 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class Ex_11 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int num=0, intervalo_0_25=0,intervalo_26_50=0,intervalo_51_75=0,intervalo_76_100=0;
+
+        while (num>=0){
+
+            // Ler num
+            System.out.print("Insira um número: ");
+            num=input.nextInt();
+
+            if(num>=0 && num<=25){
+                intervalo_0_25++; // intervalo_0_25 = intervalo_0_25 +1;
+            }
+
+            if(num>=26 && num<=50){
+                intervalo_26_50++; // intervalo_0_25 = intervalo_0_25 +1;
+            }
+
+            if(num>=51 && num<=75){
+                intervalo_51_75++; // intervalo_0_25 = intervalo_0_25 +1;
+            }
+
+            if(num>=76 && num<=100){
+                intervalo_76_100++; // intervalo_0_25 = intervalo_0_25 +1;
+            }
+
+        }
+
+        System.out.println("Intervalo [00,25]: "+intervalo_0_25);
+        System.out.println("Intervalo [26,50]: "+intervalo_26_50);
+        System.out.println("Intervalo [51,75]: "+intervalo_51_75);
+        System.out.println("Intervalo [76,100]: "+intervalo_76_100);
+    }
 }
--- FichasJava/src/FichaPratica03/Ex_12.java
+++ FichasJava/src/FichaPratica03/Ex_12.java
@@ -1,4 +1,28 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class Ex_12 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int inicio, fim;
+
+        // Ler inicio
+        System.out.print("Insira o inicio: ");
+        inicio=input.nextInt();
+
+        // Ler fim
+        System.out.print("Insira o fim: ");
+        fim=input.nextInt();
+
+        while (inicio<=fim){
+            if(inicio%5==0){
+                System.out.println(inicio);
+            }
+            inicio++;
+        }
+    }
 }
