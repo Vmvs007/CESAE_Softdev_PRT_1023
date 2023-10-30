29c4e4c5da3ef45afe519905637310494f07edc7
Vitor Santos <vmvs007@gmail.com>
2023-10-30 11:53:25 +0000

    FichaPratica03

--- FichasJava/src/FichaPratica03/Ex_09.java
+++ FichasJava/src/FichaPratica03/Ex_09.java
@@ -1,4 +1,23 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class Ex_09 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int contador=2, limite;
+
+        // Ler limite
+        System.out.print("Insira o limite: ");
+        limite= input.nextInt();
+
+        while (contador<=limite){
+            System.out.println(contador);
+            contador=contador+2;
+        }
+
+    }
 }
--- FichasJava/src/FichaPratica03/Ex_10.java
+++ FichasJava/src/FichaPratica03/Ex_10.java
@@ -1,4 +1,26 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class Ex_10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int contador=0, salto, limite;
+
+        // Ler limite
+        System.out.print("Insira o limite: ");
+        limite= input.nextInt();
+
+        // Ler salto
+        System.out.print("Insira o salto: ");
+        salto=input.nextInt();
+
+        while (contador<=limite){
+            System.out.println(contador);
+            contador=contador+salto;
+        }
+
+    }
 }
