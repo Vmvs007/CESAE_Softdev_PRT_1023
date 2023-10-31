1a0afc2721cfaeda7363801c786aeae1aae94897
Vitor Santos <vmvs007@gmail.com>
2023-10-31 09:39:48 +0000

    FichaPraticaExtraCiclos

---
+++ FichasJava/src/FichaPraticaExtra_Ciclos/Ex_10.java
@@ -0,0 +1,29 @@
+package FichaPraticaExtra_Ciclos;
+
+import java.util.Scanner;
+
+public class Ex_10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+// Declarar variáveis
+        int numero, numeroInvertido=0, digito=0;
+
+        // Ler numero
+        System.out.print("Digite um número inteiro: ");
+        numero = input.nextInt();
+
+        while (numero != 0) {
+            // Obtém o último dígito
+            digito = numero % 10;
+
+            // Acrescenta o dígito invertido
+            numeroInvertido = numeroInvertido * 10 + digito;
+
+            // Remove o último dígito
+            numero /= 10;
+        }
+
+        System.out.println("Resultado: " + numeroInvertido);
+    }
+}
