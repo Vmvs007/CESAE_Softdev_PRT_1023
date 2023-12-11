081a0f86ec55c04ef906cc8ae7ee2d055c724cd3
Vitor Santos <vmvs007@gmail.com>
2023-12-11 12:53:58 +0000

    Factory Method

---
+++ FactoryMethod/src/Ex_01/Comida.java
@@ -0,0 +1,13 @@
+package Ex_01;
+
+public class Comida extends Produto{
+
+    public Comida(String nome, double preco) {
+        super(nome, preco);
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println("Produto Categoria Comida: " + super.getNome());
+    }
+}
---
+++ FactoryMethod/src/Ex_01/Eletronico.java
@@ -0,0 +1,13 @@
+package Ex_01;
+
+public class Eletronico extends Produto{
+
+    public Eletronico(String nome, double preco) {
+        super(nome, preco);
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println("Produto Categoria Eletrónico: " + super.getNome());
+    }
+}
